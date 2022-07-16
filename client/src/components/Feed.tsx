d3bd41f5c4f4f9791f764738706e78c33d9300bd
PizzaPrix <seanwaterloo2997@gmail.com>
2022-07-15 20:42:28 -0400

    Feed works with interface array

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { setConstantValue } from 'typescript';
 import post1 from "../assets/1.png"
+import post2 from "../assets/2.png"
+import post3 from "../assets/3.png"
 
 interface Post {
   images: string[],
@@ -8,27 +9,35 @@ interface Post {
   from: string,
 }
 const Feed = () => {
-  const [postsState, setPost] = useState<Post[]>([{
-    images: ["../assets/1.png"],
-    caption: "Yo",
-    from: "",
-  }]);
+  const [postsState, setPost] = useState<Post[]>([
+    {
+      images: [post1],
+      caption: "Yo",
+      from: "Sean Wang",
+    },
+    {
+      images: [post2],
+      caption: "Happy Birthday [Ce][S]ara[h]!",
+      from: "Leanne Kim",
+    }
+  ]);
 
+  // Render each post in the postsState array
   const posts = postsState.map((postsState, i) => {
-    console.log(i);
     return (
-      <li key={i} className="text-orange-500 bg-cyan-500 px-5 border-3 border-green-500 border-solid">
-        {/* <img src="../assets/1.png">Post</img> */}
-        <p>{postsState.caption}</p>
+      // Get the border 
+      <li key={i}>
+        <img src={postsState.images[0]} alt="post" className="w-1/2 m-auto" />
+        <p className="text-center m-auto w-1/2 px-5 rounded-md border-4 border-green-500 border-solid">{postsState.caption}</p>
+        <p className="text-center m-auto px-5 borde width-5">Posted by: {postsState.from}</p>
+        <br /><br />
       </li>
     );
   });
     
   return (
     <div>
-      <p>bow chicka wow wow</p>
       <ul>
-        <img src={post1} alt="post" className="w-3/4 m-auto" />
         {posts}
       </ul>
     </div>
