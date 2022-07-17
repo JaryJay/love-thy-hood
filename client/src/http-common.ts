61b1aaf05e9114db91ff5039f406700b91fdb5f4
Dissident <kak100kak@gmail.com>
2022-07-17 10:55:53 -0400

    Update http-common.ts

--- client/src/http-common.ts
+++ client/src/http-common.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-3-144-185-14.us-east-2.compute.amazonaws.com",
-  // baseURL: "http://localhost:5000",
+  baseURL:
+    "https://api.codetabs.com/v1/proxy/?quest=http://ec2-3-144-185-14.us-east-2.compute.amazonaws.com",
   headers: {
     "Content-type": "application/json",
   },
