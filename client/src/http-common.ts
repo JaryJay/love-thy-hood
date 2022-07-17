841d459b474e9415badbd3f7b2d4e141d97e659d
JaryJay <jay.jrjren@gmail.com>
2022-07-17 01:22:51 -0400

    Finish Post Page :tada:

--- client/src/http-common.ts
+++ client/src/http-common.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://ec2-3-144-185-14.us-east-2.compute.amazonaws.com",
+  // baseURL: "http://localhost:5000",
   headers: {
     "Content-type": "application/json",
   },
