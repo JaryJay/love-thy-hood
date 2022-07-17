9b33126607dd0bac5521c611a99745263997d0bc
Leanne <108140290+SooyeunKim@users.noreply.github.com>
2022-07-17 10:21:22 +0900

    changed the profilepage

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -1,28 +1,41 @@
 import React from 'react'
-import { FaUser } from "react-icons/fa";
+import pfp from "./pfp.png";
 
 const ProfilePage = ({ user }: { user: any }) => {
   return (
-    <div className='gradient-2 h-screen items-center pt-8 text-white'>
-      <div className='lg:mx-80 md:mx-40 sm:mx-20 p-8 glass-morphism-1'>
-        <div className='flex my-2 p-2'>
-          <div className='flex-1 justify-center'>
-            {/* profile picture */}
-            pfp
-            <div className='w-8 h-8 border border-white p-1 rounded-full items-center justify-center text-center align-middle'>
-              <FaUser className='relative' />
-            </div>
-          </div>
-          <div className='flex-1 w-4/5'>
-            Name: joe<br />
-            bio: dfkl;aldks;fldhjas
-          </div>
-        </div>
-        <div>
-          POSTS POSTS POSTS {user}
-        </div>
+    <div className='gradient-2 h-screen'>
+      <img src={pfp} alt="profile picture" style={{display: "flex", position:"absolute", top:"22%", left:"20%", width:250, height:250}}/>
+        <form>
+          <input type="text" value="name" style={{display: "flex", fontSize:30, position:"absolute", textAlign:"left",top: "25%", left:"40%"}}/>
+            <label style={{display: "flex", color:"black", fontSize:20, position:"absolute", textAlign:"left", top:"30%", left:"40%"}}>
+              <br/>
+              Pronouns:
+            </label>
+            <input type="text" value="pronoun" style={{display:"flex",position:"absolute", textAlign:"left",top:"33.8%", left:"47%", fontSize:20, height:30}}/>
+            <label style={{display: "flex", color:"black", fontSize:20, position:"absolute", textAlign:"left", top:"35%", left:"40%"}}>
+              <br/>
+              Bio:
+            </label>
+            <input type="text" value="bio" style={{display:"flex",position:"absolute", textAlign:"left",top:"38.9%", left:"43%", fontSize:20, height:30}}/>
+
+            <label style={{display: "flex", color:"black", fontSize:20, position:"absolute", textAlign:"left", top:"40%", left:"40%"}}>
+              <br/>
+              Region/Country:
+            </label>
+            <input type="text" value="region" style={{display:"flex",position:"absolute", textAlign:"left",top:"44%", left:"51%", fontSize:20, height:30}}/>
+            
+            <label style={{display: "flex", color:"black", fontSize:20, position:"absolute", textAlign:"left", top:"45%", left:"40%"}}>
+              <br/>
+              Interests:
+            </label>
+            <input type="text" value="Interests" style={{display:"flex",position:"absolute", textAlign:"left",top:"49.1%", left:"48%", fontSize:20, height:30}}/>
+
+            <label style={{display: "flex", color:"black", fontSize:25,  position:"absolute", fontWeight: "bold", textAlign:"left", top:"55%", left:"10%"}}>
+              <br/>
+              Posts
+            </label>
+        </form>
       </div>
-    </div>
   )
 }
 
